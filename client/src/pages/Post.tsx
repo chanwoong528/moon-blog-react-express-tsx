6400b7f436c0de7dce571ce793b48b50e9f2ca77
chanwoong528 <cksdnd004@naver.com>
2022-01-23 11:11:15 +0900

    11

--- client/src/pages/Post.tsx
+++ client/src/pages/Post.tsx
@@ -1,8 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 export default function Post() {
+  const [posts, setPosts] = useState([]);
+
+  const onClickFetchPost = async () => {
+    const res = await fetch("/post", {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+      credentials: "include",
+    });
+    const posts = await res.json();
+    console.log(posts);
+    if (res.ok) {
+      setPosts(posts.posts);
+    }
+  };
+
   return (
     <div>
       <h1>Post</h1>
+      <div>
+        <button onClick={onClickFetchPost}>get posts</button>
+        <div>
+          <ul>
+            {posts.map((post) => {
+              return <li key={post.id}>{post.title}</li>;
+            })}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
--- server/src/index.ts
+++ server/src/index.ts
@@ -8,6 +8,16 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + "/build/index.html");
 });
 
+app.get("/post", async (req, res) => {
+  const posts = [
+    { id: 1, title: "post1", body: "post1" },
+    { id: 2, title: "post2", body: "post2" },
+    { id: 3, title: "post3", body: "post3" },
+  ];
+
+  return res.status(200).send({ posts });
+});
+
 app.listen(PORT, () => {
   console.log(`Server Running on: ${PORT}`);
 });
